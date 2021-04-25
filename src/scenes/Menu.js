c5f775ff50cb682200ef1c9a38b99c56719fd9a8
KristopherYu <74690002+KristopherYu@users.noreply.github.com>
2021-04-25 16:24:59 -0700

    Added temp background

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -7,10 +7,21 @@ class Menu extends Phaser.Scene {
         // load sprites/images
         this.load.image('ground', './assets/dirtTile.png');
         this.load.image('player', './assets/player.png');
+        this.load.image('back1', './assets/tempBackground1.png');
+        this.load.image('back2', './assets/tempBackground2.png');
+        this.load.image('back3', './assets/tempBackground3.png');
+        this.load.image('back4', './assets/tempBackground4.png');
+        this.load.image('back5', './assets/tempBackground5.png');
     }
     create() {
         //Set background color
         this.cameras.main.setBackgroundColor('#d6b894'); 
+        //set background
+        this.back1 = this.add.tileSprite(0, 0, 800, 480, 'back1').setOrigin(0, 0);
+        this.back2 = this.add.tileSprite(0, 0, 800, 480, 'back2').setOrigin(0, 0);
+        this.back3 = this.add.tileSprite(0, 0, 800, 480, 'back3').setOrigin(0, 0);
+        this.back4 = this.add.tileSprite(0, 0, 800, 480, 'back4').setOrigin(0, 0);
+        this.back5 = this.add.tileSprite(0, 0, 800, 480, 'back5').setOrigin(0, 0);
         //Set Temporary text boxes until buttons are made
         let menuConfig = {
             fontFamily: 'Courier',
@@ -35,8 +46,11 @@ class Menu extends Phaser.Scene {
             this.scene.start('playScene');
         });
     }
-    update(){
-        //temporary start
-        //this.scene.start('playScene');
+    update(time, delta){
+        let deltaMultiplier = (delta/16.66667);
+        this.back1.tilePositionX -= 0.05*deltaMultiplier;
+        this.back2.tilePositionX -= 0.07*deltaMultiplier;
+        this.back3.tilePositionX -= 0.1*deltaMultiplier;
+        this.back4.tilePositionX -= 0.15*deltaMultiplier;
     }
 }
